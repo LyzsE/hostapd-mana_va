8ad3a2a88e1d36243977ff87fcfe8e281e503022
LyzsE <115947186+LyzsE@users.noreply.github.com>
2023-11-08 09:20:13 +0300

    fix

--- src/ap/assoc.h
+++ src/ap/assoc.h
@@ -3,9 +3,9 @@
 
 struct ieee80211_mgmt;
 
-static inline void handle_assoc(struct hostapd_data *hapd,
+void handle_assoc(struct hostapd_data *hapd,
 			 const struct ieee80211_mgmt *mgmt, size_t len,
 			 int reassoc);
-static inline void sta_track_claim_taxonomy_info_assoc(struct hostapd_iface *iface, const u8 *addr,
+void sta_track_claim_taxonomy_info_assoc(struct hostapd_iface *iface, const u8 *addr,
 				   struct wpabuf **assoc_ie_taxonomy);
 #endif /* ASSOC_H */
