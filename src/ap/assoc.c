589fab4e056c7a7991a811205c5d964164816cd3
LyzsE <115947186+LyzsE@users.noreply.github.com>
2023-11-13 22:10:10 +0300

    Update assoc.c

--- src/ap/assoc.c
+++ src/ap/assoc.c
@@ -911,7 +911,7 @@ void handle_assoc(struct hostapd_data *hapd,
 {
 	
 	printf("\n\**handle_assoc**\n\n");
-	printf("\nht_capab: %s\n",hapd->iconf->ht_capab);
+	printf("\nht_capab: %u\n",hapd->iconf->ht_capab);
 	struct ieee802_11_elems elems;
 	const u8 *ie;
 	int noack;
